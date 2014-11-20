4c263aabd9617e85088e5cbd2b331de4e0f9dc03
Ariel <ariel.merino@usach.cl>
2014-11-20 15:14:53 -0300

    se cargan los templates iniciales

---
+++ sacpyc/sacpyc/static/sacpyc/js/index.js
@@ -0,0 +1,42 @@
+document.getElementById('np').className="btn btn-default active";
+document.getElementById('coffe-break').style.display='block';
+document.getElementById('catering').style.display='none';
+document.getElementById('cumpleano').style.display='none';
+document.getElementById('cena').style.display='none';
+document.getElementById('matrimonio').style.display='none';
+
+function menuLat1(){
+	document.getElementById('coffe-break').style.display='block';
+	document.getElementById('catering').style.display='none';
+	document.getElementById('cumpleano').style.display='none';
+	document.getElementById('cena').style.display='none';
+	document.getElementById('matrimonio').style.display='none';
+}
+function menuLat2(){
+	document.getElementById('coffe-break').style.display='none';
+	document.getElementById('catering').style.display='block';
+	document.getElementById('cumpleano').style.display='none';
+	document.getElementById('cena').style.display='none';
+	document.getElementById('matrimonio').style.display='none';
+}
+function menuLat3(){
+	document.getElementById('coffe-break').style.display='none';
+	document.getElementById('catering').style.display='none';
+	document.getElementById('cumpleano').style.display='block';
+	document.getElementById('cena').style.display='none';
+	document.getElementById('matrimonio').style.display='none';
+}
+function menuLat4(){
+	document.getElementById('coffe-break').style.display='none';
+	document.getElementById('catering').style.display='none';
+	document.getElementById('cumpleano').style.display='none';
+	document.getElementById('cena').style.display='block';
+	document.getElementById('matrimonio').style.display='none';
+}
+function menuLat5(){
+	document.getElementById('coffe-break').style.display='none';
+	document.getElementById('catering').style.display='none';
+	document.getElementById('cumpleano').style.display='none';
+	document.getElementById('cena').style.display='none';
+	document.getElementById('matrimonio').style.display='block';
+}
