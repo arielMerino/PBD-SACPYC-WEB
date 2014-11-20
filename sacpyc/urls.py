48587b4afbea9790a9afdf3a4d905a55178908fb
Ariel <ariel.merino@usach.cl>
2014-11-20 18:29:56 -0300

    reparando error y agregando gitignore

---
+++ sacpyc/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from .views import Home
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$','sd.views.home',name='home'), #pagina principal
+    url(r'^$',Home.as_view()),
+    url(r'^Cotizar/', include('Cotizar.urls')),
+    url(r'^Hola/',include('hola.urls'))
+)
