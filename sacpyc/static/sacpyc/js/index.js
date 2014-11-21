7e830a3def7fbb29da12b3d18467412b79eedc3e
Ariel <ariel.merino@usach.cl>
2014-11-21 14:32:44 -0300

    se eliminan carpetas inecesarias

--- sacpyc/static/sacpyc/js/index.js
+++ sacpyc/static/sacpyc/js/index.js
@@ -1,42 +1,10 @@
 document.getElementById('np').className="btn btn-default active";
-document.getElementById('coffe-break').style.display='block';
-document.getElementById('catering').style.display='none';
-document.getElementById('cumpleano').style.display='none';
-document.getElementById('cena').style.display='none';
-document.getElementById('matrimonio').style.display='none';
-
-function menuLat1(){
-	document.getElementById('coffe-break').style.display='block';
-	document.getElementById('catering').style.display='none';
-	document.getElementById('cumpleano').style.display='none';
-	document.getElementById('cena').style.display='none';
-	document.getElementById('matrimonio').style.display='none';
-}
-function menuLat2(){
-	document.getElementById('coffe-break').style.display='none';
-	document.getElementById('catering').style.display='block';
-	document.getElementById('cumpleano').style.display='none';
-	document.getElementById('cena').style.display='none';
-	document.getElementById('matrimonio').style.display='none';
-}
-function menuLat3(){
-	document.getElementById('coffe-break').style.display='none';
-	document.getElementById('catering').style.display='none';
-	document.getElementById('cumpleano').style.display='block';
-	document.getElementById('cena').style.display='none';
-	document.getElementById('matrimonio').style.display='none';
-}
-function menuLat4(){
-	document.getElementById('coffe-break').style.display='none';
-	document.getElementById('catering').style.display='none';
-	document.getElementById('cumpleano').style.display='none';
-	document.getElementById('cena').style.display='block';
-	document.getElementById('matrimonio').style.display='none';
-}
-function menuLat5(){
+botonMenu('coffe-break');
+function botonMenu(id){
 	document.getElementById('coffe-break').style.display='none';
 	document.getElementById('catering').style.display='none';
 	document.getElementById('cumpleano').style.display='none';
 	document.getElementById('cena').style.display='none';
-	document.getElementById('matrimonio').style.display='block';
+	document.getElementById('matrimonio').style.display='none';
+	document.getElementById(id).style.display='block';
 }
