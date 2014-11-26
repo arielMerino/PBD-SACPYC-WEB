59cdab8a8b903d51e87d8fc1b46742d4cf5eaea0
Ariel <ariel.merino@usach.cl>
2014-11-26 14:40:00 -0300

    primer merge

--- sacpyc/sacpyc/urls.py
+++ sacpyc/sacpyc/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from .views import Home
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',Home(0).inicio),
     url(r'^Cotizar/', include('Cotizar.urls')),
-)
+]
